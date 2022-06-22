599e9358fa044b7b72886bd5eba0c70ac68ceef9
Prem Pavuluri <52355836+premkpavuluri@users.noreply.github.com>
2022-06-22 20:00:39 +0530

    Extracted handleRequest from main

--- src/server.js
+++ src/server.js
@@ -2,13 +2,17 @@ const { createServer } = require('net');
 
 const response = 'HTTP/1.1 200 OK\r\n\r\nhello\r\n';
 
+const handleRequest = (socket, request) => {
+  console.log(request);
+  socket.write(response);
+};
+
 const main = () => {
   const server = createServer((socket) => {
     socket.setEncoding('utf8');
 
     socket.on('data', (chunk) => {
-      console.log(chunk);
-      socket.write(response);
+      handleRequest(socket, chunk);
       socket.end();
     });
   });
